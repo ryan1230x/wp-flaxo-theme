b8b0da7cc25f1721d1ac25fd0aa3d2f45c6d162a
Ryan <ryan1230tenerife@gmail.com>
2021-05-26 14:51:19 +0100

    change css and js enqueue path

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  * load css
  */
 function load_stylesheets(){
-  wp_register_style("rw_stylesheet", get_template_directory_uri() . "/dist/main.css", "", 1);
+  wp_register_style("rw_stylesheet", get_template_directory_uri() . "/main.css", "", 1);
   wp_enqueue_style("rw_stylesheet");
 }
 add_action("wp_enqueue_scripts", "load_stylesheets");
@@ -13,7 +13,7 @@ add_action("wp_enqueue_scripts", "load_stylesheets");
  * load js
  */
 function load_scripts(){
-  wp_register_script("rw_javacript", get_template_directory_uri() . "/dist/main.js", "", 1, true);
+  wp_register_script("rw_javacript", get_template_directory_uri() . "/main.js", "", 1, true);
   wp_enqueue_script("rw_javacript");
 }
 add_action("wp_enqueue_scripts", "load_scripts");
